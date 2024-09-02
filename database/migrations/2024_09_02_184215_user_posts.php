7b49ff6ed6c89f925e1357e44020d8c3d275dfcd
Jacob Heyes <heyesjacob@gmail.com>
2024-09-02 19:46:35 +0100

    initial commit

---
+++ database/migrations/2024_09_02_184215_user_posts.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\post;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('user_post', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(post::class);
+            $table->string('title');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
