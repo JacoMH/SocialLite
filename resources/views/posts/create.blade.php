58c06ac9027467edb4d2d0ba500d3e769fb14cb1
Jacob Heyes <heyesjacob@gmail.com>
2024-09-16 16:42:51 +0100

    dashboard removed, welcome page changed, some css adjustments

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,4 +1,3 @@
-<script type='text/javascript' src='{{ asset('resources\js\test.js')}}' defer></script>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
